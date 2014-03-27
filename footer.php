a6c7d8239bc93238dd0e02b3a70aa9b28bfe5eb2
claudiuvertistudio <claudiu.buruiana@vertistudio.com>
2014-03-27 16:31:31 +0200

    am facut css-ul la sidebar

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-		<div class="clearfix"></div>		<footer>			<?php _e('All rights reserved.','cwp'); ?>  &#169; <?php echo get_the_date("Y"); ?> - <?php echo date("Y"); ?> - <?php bloginfo('name'); ?>		</footer>	<?php wp_footer(); ?>	</body></html>
+		<div class="left-sidebar sidebar-mobile">			<?php get_sidebar(mobile); ?>		</div>		<div class="clearfix"></div>		<footer>			<?php _e('All rights reserved.','cwp'); ?>  &#169; <?php echo get_the_date("Y"); ?> - <?php echo date("Y"); ?> - <?php bloginfo('name'); ?>		</footer>	<?php wp_footer(); ?>	</body></html>
