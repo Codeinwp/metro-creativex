17a98207a5b158847cb761fd94a646c70ce220b6
rodica-andronache <rodik_andronache@yahoo.com>
2014-03-27 13:54:09 +0200

    am modificat licenta din style.css, am sanitizat datele cu esc_url_raw, am prefixat toate scripturile din functions cu acelasi prefix, am inclus sidebar, am folosit filter pentru toate post_class pt post formats, am inclus folderul languages

--- inc/customizer.php
+++ inc/customizer.php
@@ -23,7 +23,7 @@ function cwp_customize_register( $wp_customize ) {
     	'description' => __('Upload a logo to replace the default site name and description in the header','cwp'),
 	) );
 
-	$wp_customize->add_setting( 'codeinwp_logo', array('default' => get_template_directory_uri().'/images/logo.png') );
+	$wp_customize->add_setting( 'codeinwp_logo' );
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
 	    'label'    => __( 'Logo', 'cwp' ),
 	    'section'  => 'codeinwp_logo_section',
@@ -37,14 +37,14 @@ function cwp_customize_register( $wp_customize ) {
     	'priority'    => 31,
 	) );
 
-	$wp_customize->add_setting( 'codeinwp_social_link_fb', array('sanitize_callback' => 'cwp_sanitize_text') );
+	$wp_customize->add_setting( 'codeinwp_social_link_fb', array('sanitize_callback' => 'esc_url_raw') );
 	$wp_customize->add_control( 'codeinwp_social_link_fb', array(
 	    'label'    => __( 'Facebook link', 'cwp' ),
 	    'section'  => 'codeinwp_socials_section',
 	    'settings' => 'codeinwp_social_link_fb',
 		'priority'    => 5,
 	) );
-	$wp_customize->add_setting( 'codeinwp_social_link_tw', array('sanitize_callback' => 'cwp_sanitize_text') );
+	$wp_customize->add_setting( 'codeinwp_social_link_tw', array('sanitize_callback' => 'esc_url_raw') );
 	$wp_customize->add_control( 'codeinwp_social_link_tw', array(
 	    'label'    => __( 'Twitter link', 'cwp' ),
 	    'section'  => 'codeinwp_socials_section',
@@ -54,7 +54,3 @@ function cwp_customize_register( $wp_customize ) {
 
 }
 add_action( 'customize_register', 'cwp_customize_register' );
-
-function cwp_sanitize_text( $input ) {
-    return wp_kses_post( force_balance_tags( $input ) );
-}
