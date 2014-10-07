24e107d34a78baf953f2e9247a936b9884cb1890
mariusalex20 <cristea.marius.alexandru@gmail.com>
2014-10-07 14:22:55 +0300

    sync with wp.org

--- functions.php
+++ functions.php
@@ -3,13 +3,11 @@
  * Set content width
  */
 
-function metrox_setup() {
 
-	if ( ! isset( $content_width ) ) 
-		$content_width = 600;
-
+function cwp_setup() {
+	global $content_width;
+	if ( ! isset( $content_width ) ) $content_width = 600;
 	load_theme_textdomain( 'metrox', get_template_directory() . '/languages' );
-	
 	/*
 	* Register menus
 	*/
@@ -18,23 +16,22 @@ function metrox_setup() {
 			'secound' => __( 'Header menu', 'metrox')
 		)
 	);
-	
-	/* Add theme support for Featured Images */
-	
-	add_theme_support( 'post-thumbnails' );	
+	// Add theme support for Featured Images
+	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'automatic-feed-links' );
-	
     /**
      * Enable support for Post Formats
      */
     add_theme_support( 'post-formats', array( 'aside', 'gallery','link','image','quote','status','video','audio','chat' ) );
-	
+
+    add_editor_style( '/css/custom-editor-style.css' );
 	/* custom background */
 	$args = array(
 	  'default-color' => '000000'
 	);
 	add_theme_support( 'custom-background', $args );
-	
+
+    require get_template_directory() . '/inc/customizer.php';
 	/* custom header */
 	$args = array(
 		'width'         => 980,
@@ -43,24 +40,53 @@ function metrox_setup() {
 		'uploads'       => true,
 	);
 	add_theme_support( 'custom-header', $args );
-	
-	register_sidebar( array(
-		'name'         => __( 'Left sidebar' ),
-		'id'           => 'sidebar-1',
-		'description'  => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside><br style="clear:both">',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-	) );
 
-}		
-add_action( 'after_setup_theme', 'metrox_setup' ); 
+}
+function cwp_register_sidebar(){
+
+
+    register_sidebar( array(
+            'name'         => __( 'Left sidebar','metrox' ),
+            'id'           => 'sidebar-1',
+            'description'  => '',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</aside><br style="clear:both">',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+    ) );
+
+}
+add_action( 'widgets_init',  "cwp_register_sidebar");
+
+add_action( 'after_setup_theme', 'cwp_setup' );
+
+// Custom title function
+function cwp_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'metrox' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'cwp_wp_title', 10, 2 );
 
 /**
  * Returns the URL from the post.
  */
-function metrox_link_post_format() {
+function cwp_link_post_format() {
 	$content = get_the_content();
 	$has_url = get_url_in_content( $content );
 	return ( $has_url ) ? $has_url : apply_filters( 'the_permalink', get_permalink() );
@@ -68,48 +94,37 @@ function metrox_link_post_format() {
 /**
  * Enqueue scripts and styles
  */
-function metrox_theme_scripts() {	
-
+function cwp_theme_scripts() {
     $blog_url = home_url();
-	
-	wp_enqueue_style( 'metrox_theme_style', get_stylesheet_uri() );
-	
-	wp_register_style( 'metrox_opensans_font', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-	wp_enqueue_style( 'metrox_opensans_font' );
-	
-	wp_register_style( 'metrox_sourcesans_font', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic');
-	wp_enqueue_style( 'metrox_sourcesans_font' );		
-    
-	wp_enqueue_script('jquery');
-	
-    wp_enqueue_script( 'metrox_jscript', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true );
-	
-    wp_enqueue_script( 'metrox_carouFredSel', get_template_directory_uri() . '/js/jquery.carouFredSel-6.1.0.js', array('jquery'), '6.1', true );
+
+	wp_enqueue_style( 'cwp_theme-style', get_stylesheet_uri() );
+
+	wp_register_style( 'cwp_opensans-font', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
+	wp_enqueue_style( 'cwp_opensans-font' );
+	wp_register_style( 'cwp_sourcesans-font', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic');
+	wp_enqueue_style( 'cwp_sourcesans-font' );
+	wp_enqueue_script( 'tinynav', get_template_directory_uri() . '/js/tinynav.min.js', array('jquery'), 'v1.1', true );
+    wp_enqueue_script( 'cwp_jscript', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true );
+
+    wp_enqueue_script( 'cwp_carouFredSel', get_template_directory_uri() . '/js/jquery.carouFredSel-6.1.0.js', array('jquery'), '6.1', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 	if ( !is_single() and !is_page() ) {
-        wp_enqueue_script( 'metrox_jmasonry', get_template_directory_uri() . '/js/masonry.pkgd.js', array('jquery'), '3.6.0', true );
+        wp_enqueue_script( 'jquery-masonry');
     }
-      
-}add_action( 'wp_enqueue_scripts', 'metrox_theme_scripts' );
-/**
- * No title
- */
-add_filter( 'the_title', 'metrox_default_title' ); 
-function metrox_default_title( $title ) {
-	if($title == '')
-		$title = "Default title";
-	return $title;
+
 }
+add_action( 'wp_enqueue_scripts', 'cwp_theme_scripts' );
+
 /**
  * Remove Gallery shortcode css style
  */
-add_filter( 'use_default_gallery_style', '__return_false' ); 
+add_filter( 'use_default_gallery_style', '__return_false' );
 /**
  * Displays navigation to next/previous set of posts when applicable.
  */
-function metrox_pagination() {
+function cwp_pagination() {
 	global $wp_query;
 	// Don't print empty markup if there's only one page.
 	if ( $wp_query->max_num_pages < 2 )
@@ -117,29 +132,29 @@ function metrox_pagination() {
 	?>
 	<article class="navigation" role="navigation">
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'wwbw' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'metrox' ) ); ?></div>
 			<?php endif; ?>
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'wwbw' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'metrox' ) ); ?></div>
 			<?php endif; ?>
 	</article><!-- .navigation -->
 	<?php
-} 
+}
 /**
  * Template for comments and pingbacks.
  *
  * To override this walker in a child theme without modifying the comments template
- * simply create your own metrox_comment(), and that function will be used instead.
+ * simply create your own cwp_comment(), and that function will be used instead.
  *
  * Used as a callback by wp_list_comments() for displaying the comments.
  *
+ * @since cwp 1.0
  */
- 
- 
-require get_template_directory() . '/inc/customizer.php';
 
 
-function metrox_comment( $comment, $args, $depth ) {
+
+
+function cwp_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
 		case 'pingback' :
@@ -179,46 +194,41 @@ function metrox_comment( $comment, $args, $depth ) {
 					<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'metrox' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => 20 ) ) ); ?>
 				</div><!-- .reply -->
 			</div><!--/comm_content-->
-			
+
 		</div><!--/comment-->
 	<?php
 		break;
 	endswitch; // end comment_type check
 }
 
-function metrox_add_editor_styles() {
-    add_editor_style( '/css/custom-editor-style.css' );
-}
-add_action( 'init', 'metrox_add_editor_styles' );
 
+add_filter( 'post_class', 'cwp_post_class' );
 
-add_filter( 'post_class', 'metrox_post_class' );
- 
-function metrox_post_class( $classes ){
+ function cwp_post_class( $classes ){
 	global $post;
-	
+
 	if(is_single($post->ID)):
 		$class[] = 'post';
 	else:
-		$format = get_post_format($post->ID);	
+		$format = get_post_format($post->ID);
 		if($format == 'aside'):
 			$class[] = 'bg-design';
 		elseif(($format == 'audio') || ($format == 'video')):
 			$class[] = 'bg-wordpress';
-		elseif(($format == 'gallery') || ($format == 'image')):	
+		elseif(($format == 'gallery') || ($format == 'image')):
 			$class[] = 'bg-responsive';
 		elseif(($format == 'link') || ($format == 'quote') || ($format == 'status')):
 			$class[] = 'bg-web';
-		else:	
+		else:
 			$class[] = 'bg-stuff';
 		endif;
-	endif;	
-	 
+	endif;
+
 	return $class;
- 
+
  }
- 
-function metrox_excerpt_max_charlength($charlength) {
+
+function cwp_excerpt_max_charlength($charlength) {
 	$excerpt = get_the_excerpt();
 	$charlength++;
 
@@ -235,4 +245,5 @@ function metrox_excerpt_max_charlength($charlength) {
 	} else {
 		echo $excerpt;
 	}
-} 
+}
+
--- inc/customizer.php
+++ inc/customizer.php
@@ -2,7 +2,7 @@
 /**
  * Theme Customizer
  *
- * @package metrox
+ * @package cwp
  */
 
 /**
@@ -10,75 +10,76 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function metrox_customize_register( $wp_customize ) {
+function cwp_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-	
+
 	$wp_customize->remove_section( 'background_image' );
 	$wp_customize->remove_control('header_textcolor');
-	
+
 	/* theme notes */
-	$wp_customize->add_section( 'metrox_theme_notes' , array(
-		'title'      => __('ThemeIsle theme notes','metrox'),
+	$wp_customize->add_section( 'codeinwp_theme_notes' , array(
+		'title'      => __('ThemeIsle theme notes','codeinwp'),
 		'description' => sprintf( __( "Thank you for being part of this! We've spent almost 6 months building ThemeIsle without really knowing if anyone will ever use a theme or not, so we're very grateful that you've decided to work with us. Wanna <a href='http://themeisle.com/contact/' target='_blank'>say hi</a>?
-		<br/><br/><a href='http://themeisle.com/demo/?theme=MetroX' target='_blank' />View Theme Demo</a> | <a href='http://themeisle.com/forums/forum/metrox' target='_blank'>Get theme support</a><br/><br/><a href='http://themeisle.com/documentation-metrox' target='_blank'>Documentation</a>")),
+		<br/><br/><a href='http://themeisle.com/demo/?theme=MetroX' target='_blank' />View Theme Demo</a> | <a href='http://themeisle.com/forums/forum/metrox/' target='_blank'>Get theme support</a>")),
 		'priority'   => 30,
 	));
 	$wp_customize->add_setting(
-        'metrox_theme_notice'
+        'cwp_theme_notice'
 	);
-	
+
 	$wp_customize->add_control(
-    'metrox_theme_notice',
+    'cwp_theme_notice',
     array(
-        'section' => 'metrox_theme_notes',
+        'section' => 'codeinwp_theme_notes',
 		'type'  => 'read-only',
     ));
-	
-	/* logo */
-	$wp_customize->add_section( 'metrox_logo_section' , array(
+
+	$wp_customize->add_section( 'codeinwp_logo_section' , array(
     	'title'       => __( 'Logo', 'metrox' ),
     	'priority'    => 30,
     	'description' => __('Upload a logo to replace the default site name and description in the header','metrox'),
 	) );
 
-	$wp_customize->add_setting( 'metrox_logo' );
+	$wp_customize->add_setting( 'codeinwp_logo',
+        array('sanitize_callback' => 'esc_url_raw') );
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'themeslug_logo', array(
 	    'label'    => __( 'Logo', 'metrox' ),
-	    'section'  => 'metrox_logo_section',
-	    'settings' => 'metrox_logo',
+	    'section'  => 'codeinwp_logo_section',
+	    'settings' => 'codeinwp_logo',
 	) ) );
-	
+
 
 	/* Socials */
-	$wp_customize->add_section( 'metrox_socials_section' , array(
+	$wp_customize->add_section( 'codeinwp_socials_section' , array(
     	'title'       => __( 'Socials', 'metrox' ),
     	'priority'    => 31,
 	) );
 
-	$wp_customize->add_setting( 'metrox_social_link_fb', array('sanitize_callback' => 'esc_url_raw') );
-	$wp_customize->add_control( 'metrox_social_link_fb', array(
+	$wp_customize->add_setting( 'codeinwp_social_link_fb', array('sanitize_callback' => 'esc_url_raw') );
+	$wp_customize->add_control( 'codeinwp_social_link_fb', array(
 	    'label'    => __( 'Facebook link', 'metrox' ),
-	    'section'  => 'metrox_socials_section',
-	    'settings' => 'metrox_social_link_fb',
+	    'section'  => 'codeinwp_socials_section',
+	    'settings' => 'codeinwp_social_link_fb',
+
 		'priority'    => 5,
 	) );
-	$wp_customize->add_setting( 'metrox_social_link_tw', array('sanitize_callback' => 'esc_url_raw') );
-	$wp_customize->add_control( 'metrox_social_link_tw', array(
+	$wp_customize->add_setting( 'codeinwp_social_link_tw', array('sanitize_callback' => 'esc_url_raw') );
+	$wp_customize->add_control( 'codeinwp_social_link_tw', array(
 	    'label'    => __( 'Twitter link', 'metrox' ),
-	    'section'  => 'metrox_socials_section',
-	    'settings' => 'metrox_social_link_tw',
+	    'section'  => 'codeinwp_socials_section',
+	    'settings' => 'codeinwp_social_link_tw',
 		'priority'    => 10,
 	) );
 
 }
-add_action( 'customize_register', 'metrox_customize_register' );
+add_action( 'customize_register', 'cwp_customize_register' );
 
 /**
  * Bind JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function metrox_customize_preview_js() {
+function cwp_customize_preview_js() {
 	wp_enqueue_script( 'customizerJS', get_template_directory_uri() . '/js/customizer.js', array( 'jquery' ), '20131205', true );
 }
-add_action( 'customize_preview_init', 'metrox_customize_preview_js' );
+add_action( 'customize_preview_init', 'cwp_customize_preview_js' );
