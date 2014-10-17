c0c7698dde4751d6991f0fb9b6938210b63a1895
rodica <rodik_andronache@yahoo.com>
2014-10-16 23:11:30 -0700

    Changed text domain and small fixes

--- functions.php
+++ functions.php
@@ -3,17 +3,20 @@
  * Set content width
  */
 
+function metro_creativex_setup() {
 
-function cwp_setup() {
 	global $content_width;
+	
 	if ( ! isset( $content_width ) ) $content_width = 600;
-	load_theme_textdomain( 'metrox', get_template_directory() . '/languages' );
+	
+	load_theme_textdomain( 'metro-creativex', get_template_directory() . '/languages' );
+	
 	/*
 	* Register menus
 	*/
 	register_nav_menus(
 		array(
-			'secound' => __( 'Header menu', 'metrox')
+			'secound' => __( 'Header menu', 'metro-creativex')
 		)
 	);
 	// Add theme support for Featured Images
@@ -42,11 +45,11 @@ function cwp_setup() {
 	add_theme_support( 'custom-header', $args );
 
 }
-function cwp_register_sidebar(){
+function metro_creativex_register_sidebar(){
 
 
     register_sidebar( array(
-            'name'         => __( 'Left sidebar','metrox' ),
+            'name'         => __( 'Left sidebar','metro-creativex' ),
             'id'           => 'sidebar-1',
             'description'  => '',
             'before_widget' => '<aside id="%1$s" class="widget %2$s">',
@@ -56,12 +59,12 @@ function cwp_register_sidebar(){
     ) );
 
 }
-add_action( 'widgets_init',  "cwp_register_sidebar");
+add_action( 'widgets_init',  "metro_creativex_register_sidebar");
 
-add_action( 'after_setup_theme', 'cwp_setup' );
+add_action( 'after_setup_theme', 'metro_creativex_setup' );
 
 // Custom title function
-function cwp_wp_title( $title, $sep ) {
+function metro_creativex_wp_title( $title, $sep ) {
 	global $paged, $page;
 
 	if ( is_feed() )
@@ -77,16 +80,16 @@ function cwp_wp_title( $title, $sep ) {
 
 	// Add a page number if necessary.
 	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'metrox' ), max( $paged, $page ) );
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'metro-creativex' ), max( $paged, $page ) );
 
 	return $title;
 }
-add_filter( 'wp_title', 'cwp_wp_title', 10, 2 );
+add_filter( 'wp_title', 'metro_creativex_wp_title', 10, 2 );
 
 /**
  * Returns the URL from the post.
  */
-function cwp_link_post_format() {
+function metro_creativex_link_post_format() {
 	$content = get_the_content();
 	$has_url = get_url_in_content( $content );
 	return ( $has_url ) ? $has_url : apply_filters( 'the_permalink', get_permalink() );
@@ -94,19 +97,19 @@ function cwp_link_post_format() {
 /**
  * Enqueue scripts and styles
  */
-function cwp_theme_scripts() {
+function metro_creativex_theme_scripts() {
     $blog_url = home_url();
 
-	wp_enqueue_style( 'cwp_theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'metro_creativex-style', get_stylesheet_uri() );
 
-	wp_register_style( 'cwp_opensans-font', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-	wp_enqueue_style( 'cwp_opensans-font' );
-	wp_register_style( 'cwp_sourcesans-font', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic');
-	wp_enqueue_style( 'cwp_sourcesans-font' );
-	wp_enqueue_script( 'tinynav', get_template_directory_uri() . '/js/tinynav.min.js', array('jquery'), 'v1.1', true );
-    wp_enqueue_script( 'cwp_jscript', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true );
+	wp_enqueue_style( 'metro_creativex_opensans-font', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
+	
+	wp_enqueue_style( 'metro_creativex_sourcesans-font', '//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic');
+	
+	wp_enqueue_script( 'metro_creativex_tinynav', get_template_directory_uri() . '/js/tinynav.min.js', array('jquery'), 'v1.1', true );
+    wp_enqueue_script( 'metro_creativex_jscript', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true );
 
-    wp_enqueue_script( 'cwp_carouFredSel', get_template_directory_uri() . '/js/jquery.carouFredSel-6.1.0.js', array('jquery'), '6.1', true );
+    wp_enqueue_script( 'metro_creativex_carouFredSel', get_template_directory_uri() . '/js/jquery.carouFredSel-6.1.0.js', array('jquery'), '6.1', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -115,7 +118,7 @@ function cwp_theme_scripts() {
     }
 
 }
-add_action( 'wp_enqueue_scripts', 'cwp_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'metro_creativex_theme_scripts' );
 
 /**
  * Remove Gallery shortcode css style
@@ -124,7 +127,7 @@ add_filter( 'use_default_gallery_style', '__return_false' );
 /**
  * Displays navigation to next/previous set of posts when applicable.
  */
-function cwp_pagination() {
+function metro_creativex_pagination() {
 	global $wp_query;
 	// Don't print empty markup if there's only one page.
 	if ( $wp_query->max_num_pages < 2 )
@@ -132,10 +135,10 @@ function cwp_pagination() {
 	?>
 	<article class="navigation" role="navigation">
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'metrox' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'metro-creativex' ) ); ?></div>
 			<?php endif; ?>
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'metrox' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'metro-creativex' ) ); ?></div>
 			<?php endif; ?>
 	</article><!-- .navigation -->
 	<?php
@@ -144,17 +147,17 @@ function cwp_pagination() {
  * Template for comments and pingbacks.
  *
  * To override this walker in a child theme without modifying the comments template
- * simply create your own cwp_comment(), and that function will be used instead.
+ * simply create your own metro_creativex_comment(), and that function will be used instead.
  *
  * Used as a callback by wp_list_comments() for displaying the comments.
  *
- * @since cwp 1.0
+ * @since metro-creativex 1.0
  */
 
 
 
 
-function cwp_comment( $comment, $args, $depth ) {
+function metro_creativex_comment( $comment, $args, $depth ) {
 	$GLOBALS['comment'] = $comment;
 	switch ( $comment->comment_type ) :
 		case 'pingback' :
@@ -162,7 +165,7 @@ function cwp_comment( $comment, $args, $depth ) {
 		// Display trackbacks differently than normal comments.
 	?>
 	<li <?php comment_class(); ?> id="comment-<?php comment_ID(); ?>">
-		<p><?php _e( 'Pingback:', 'metrox' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'metrox' ), '<span class="edit-link">', '</span>' ); ?></p>
+		<p><?php _e( 'Pingback:', 'metro-creativex' ); ?> <?php comment_author_link(); ?> <?php edit_comment_link( __( '(Edit)', 'metro-creativex' ), '<span class="edit-link">', '</span>' ); ?></p>
 	<?php
 			break;
 		default :
@@ -176,22 +179,22 @@ function cwp_comment( $comment, $args, $depth ) {
 					printf( '<cite class="fn">%1$s %2$s</cite>',
 						get_comment_author_link(),
 						// If current post author is also comment author, make it known visually.
-						( $comment->user_id === $post->post_author ) ? '<span> ' . __( '', 'metrox' ) . '</span>' : ''
+						( $comment->user_id === $post->post_author ) ? '<span> ' . __( '', 'metro-creativex' ) . '</span>' : ''
 					);
 					printf( '<b><a href="%1$s"><time datetime="%2$s">%3$s</time></a></b>',
 						esc_url( get_comment_link( $comment->comment_ID ) ),
 						get_comment_time( 'c' ),
 						/* translators: 1: date, 2: time */
-						sprintf( __( '%1$s at %2$s', 'metrox' ), get_comment_date(), get_comment_time() )
+						sprintf( __( '%1$s at %2$s', 'metro-creativex' ), get_comment_date(), get_comment_time() )
 					);
 				?></span>
 				<?php comment_text(); ?>
 				<?php if ( '0' == $comment->comment_approved ) : ?>
-					<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'metrox' ); ?></p>
+					<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'metro-creativex' ); ?></p>
 				<?php endif; ?>
-				<?php edit_comment_link( __( 'Edit', 'metrox' ), '<p class="edit-link">', '</p>' ); ?>
+				<?php edit_comment_link( __( 'Edit', 'metro-creativex' ), '<p class="edit-link">', '</p>' ); ?>
 				<div class="reply">
-					<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'metrox' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => 20 ) ) ); ?>
+					<?php comment_reply_link( array_merge( $args, array( 'reply_text' => __( 'Reply', 'metro-creativex' ), 'after' => ' <span>&darr;</span>', 'depth' => $depth, 'max_depth' => 20 ) ) ); ?>
 				</div><!-- .reply -->
 			</div><!--/comm_content-->
 
@@ -202,9 +205,9 @@ function cwp_comment( $comment, $args, $depth ) {
 }
 
 
-add_filter( 'post_class', 'cwp_post_class' );
+add_filter( 'post_class', 'metro_creativex_post_class' );
 
- function cwp_post_class( $classes ){
+ function metro_creativex_post_class( $classes ){
 	global $post;
 
 	if(is_single($post->ID)):
@@ -228,7 +231,7 @@ add_filter( 'post_class', 'cwp_post_class' );
 
  }
 
-function cwp_excerpt_max_charlength($charlength) {
+function metro_creativex_excerpt_max_charlength($charlength) {
 	$excerpt = get_the_excerpt();
 	$charlength++;
 
